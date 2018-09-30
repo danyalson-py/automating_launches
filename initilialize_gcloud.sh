418e8650446dc3f776c37416b91632e13c3a25e1
cloudmining <cloudminingscript@protonmail.com>
2018-09-30 17:41:22 -0300

    renaming

--- initilialize_gcloud.sh
+++ initilialize_gcloud.sh
@@ -21,7 +21,7 @@ quota_per_group=8
 projects=5
 billing_account="$(gcloud beta billing accounts list --format 'value(ACCOUNT_ID)')"
 current_project="$(gcloud config list --format 'value(core.project)')"
-
+regions=(us-east1 us-east4 us-west1 us-west2)
 random-string()
 {
 	cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 11 | head -n 1
@@ -37,11 +37,11 @@ create-instances()
 
 
 	gcloud compute instance-templates create $template_name --machine-type=n1-highcpu-2 --network-tier=PREMIUM --metadata=startup-script="$script" --no-restart-on-failure --maintenance-policy=TERMINATE --preemptible --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --image=ubuntu-minimal-1604-xenial-v20180814 --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=$template_name
-	
-	gcloud beta compute instance-groups managed create instance-group-1 --base-instance-name=instance-group-1 --template=$template_name --size=$quota_per_group --zones=us-east1-b,us-east1-c,us-east1-d
-	gcloud beta compute instance-groups managed create instance-group-2 --base-instance-name=instance-group-2 --template=$template_name --size=$quota_per_group --zones=us-east4-a,us-east4-b,us-east4-c 
-	gcloud beta compute instance-groups managed create instance-group-3 --base-instance-name=instance-group-3 --template=$template_name --size=$quota_per_group --zones=us-west1-a,us-west1-b,us-west1-c 
-	gcloud beta compute instance-groups managed create instance-group-4 --base-instance-name=instance-group-4 --template=$template_name --size=$quota_per_group --zones=us-west2-c,us-west2-b,us-west2-a 
+
+	for n in {1..4}
+	do
+		gcloud beta compute instance-groups managed create instance-group-$n --base-instance-name=instance-group-$n --template=$template_name --size=$quota_per_group --region="${regions[n-1]}"
+	done
 
 }
 
