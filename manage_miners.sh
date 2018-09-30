ae85e1aa5fa874663b1aa648d9b1cd52e1b1eb34
cloudmining <cloudminingscript@protonmail.com>
2018-09-30 18:32:50 -0300

    code styling and removed echo used to debug

--- manage_miners.sh
+++ manage_miners.sh
@@ -4,12 +4,11 @@ regions=(us-east1 us-east4 us-west1 us-west2)
 
 scale-group()
 {
-	if [ $# -ne 2 ]
-  	then
-  		echo "scale-group arg1 arg2"
-    	echo "arg1 = instance-group number"
-    	echo "arg2 = new size"
-    	return
+	if [ $# -ne 2 ]; then
+		echo "scale-group arg1 arg2"
+	    	echo "arg1 = instance-group number"
+	    	echo "arg2 = new size"
+	    	return
 	fi
 	gcloud compute instance-groups managed resize instance-group-$1 --size=$2 --region="${regions[$1-1]}"
 }
@@ -39,6 +38,6 @@ scale-project()
 		
 	for i in {1..4}
 	do
-		echo scale-group $i $n
+		scale-group $i $n
 	done
 }
